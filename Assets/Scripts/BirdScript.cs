61e551520d55896d3dbb044988be677c5deda8da
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2025-01-10 20:11:26 +0530

    Added GameOver and PlayAgain

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -4,19 +4,28 @@ public class BirdController : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birbIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameObject.name = "Angry Birb";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birbIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birbIsAlive = false;
     }
 }
